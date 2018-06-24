8eb24fea37d7b41edd0aa819aa115715ba53d004
Cecile Muller <git@wildpeaks.fr>
2018-06-24 02:06:12 +0200

    Declare Typescript modules exports

---
+++ globals.d.ts
@@ -0,0 +1,26 @@
+
+declare module 'three/examples/js/controls/OrbitControls' {
+	export const OrbitControls: typeof THREE.OrbitControls;
+}
+
+declare module 'three/examples/js/postprocessing/EffectComposer' {
+	export const EffectComposer: typeof THREE.EffectComposer;
+	export const Pass: typeof THREE.Pass;
+}
+
+declare module 'three/examples/js/postprocessing/RenderPass' {
+	export const RenderPass: typeof THREE.RenderPass;
+}
+declare module 'three/examples/js/postprocessing/ShaderPass' {
+	export const ShaderPass: typeof THREE.ShaderPass;
+}
+
+declare module 'three/examples/js/shaders/CopyShader' {
+	export const CopyShader: typeof THREE.CopyShader;
+}
+declare module 'three/examples/js/shaders/DotScreenShader' {
+	export const DotScreenShader: typeof THREE.DotScreenShader;
+}
+declare module 'three/examples/js/shaders/RGBShiftShader' {
+	export const RGBShiftShader: typeof THREE.RGBShiftShader;
+}
